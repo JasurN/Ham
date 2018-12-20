7742e25bfa0a3c32917b838777dd329da22dce2f
abdurahmonbek <abdurahmonkodirov97@gmail.com>
2018-12-20 14:17:24 +0500

    Fixed activity_main

--- app/src/main/java/e/acer_aspire/fooddeliveryservice/MainActivity.java
+++ app/src/main/java/e/acer_aspire/fooddeliveryservice/MainActivity.java
@@ -1,13 +1,35 @@
 package e.acer_aspire.fooddeliveryservice;
 
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "myLogs";
+    private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void init() {
+        mToolBar = (Toolbar) findViewById(R.id.tab_toolbar);
+        setSupportActionBar(mToolBar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
     }
 }
